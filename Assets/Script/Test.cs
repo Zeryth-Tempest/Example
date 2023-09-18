3ee672d2e72718630552e34a8d3184266187c508
FIlip Boisen <filip.boisen@elev.ga.lbs.se>
2023-09-18 16:25:02 +0200

    Initial Commit

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    int i = 50;
+    float l = 3f;
+
+
+    // Start is called before the first frame update
+    void Start() 
+    {
+        Debug.Log(i/l);
+    }
+    
+  
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
